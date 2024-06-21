e7f5f6c3bd59a3e51b771df76ec88ac6ca1b0d28
Marko Duspara <m.duspara@globalsoft.ba>
2024-06-21 09:37:42 +0200

    init

---
+++ backend/src/config/typeorm.config.ts
@@ -0,0 +1,19 @@
+import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+import { resolve } from 'path';
+
+config({ path: resolve(__dirname, '.env') });
+
+const DataSourceConfig = new DataSource({
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  migrations: ['./dist/migrations/*'],
+  synchronize: false,
+  migrationsTableName: 'migrations',
+});
+
+export default DataSourceConfig;
